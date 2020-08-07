7f86ffb4621e3857cdf5b18689b77c9ad878b7c0
Pradeep Rajkumar <pradeep.rajkumar@ganitinc.com>
2020-08-07 12:41:17 +0530

    Project lists update

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,34 +7,10 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 
 let projects = [
   {
-    projectName: "AIVIA",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    doc: "doc1",
-    imgSrc: "/img/aivia.svg"
-  },
-  {
-    projectName: "MORE",
+    projectName: "MORE (SQL)",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
     doc: "more/autoIndent/actualIndent",
-    imgSrc: "/img/more.svg"
-  },
-  {
-    projectName: "AIVIA",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    doc: "doc1",
-    imgSrc: "/img/aivia.svg"
-  },
-  {
-    projectName: "AIVIA",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    doc: "doc1",
-    imgSrc: "/img/aivia.svg"
-  },
-  {
-    projectName: "AIVIA",
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    doc: "doc1",
-    imgSrc: "/img/aivia.svg"
+    imgSrc: "/img/database.svg"
   }
 ];
 
